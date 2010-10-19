de92c4a1297c6418b583bf36412c34fdd5ae401a
Eugene Koontz <ekoontz@hiro-tan.org>
2010-10-18 23:43:27 -0700

    set java_url

--- lib/defaults.rb
+++ lib/defaults.rb
@@ -2,6 +2,7 @@ module Hadoop
   class Defaults
     @@options = {
       :base_image_name => "ami-38c33651",
+      :java_url => "http://ekoontz-tarballs.s3.amazonaws.com/jdk-6u22-linux-x64.bin"
     }
     def Defaults.options
       @@options
