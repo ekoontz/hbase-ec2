8398e21eb56bbd4f4fae1f21605bcd2ebf2ffe02
Eugene Koontz <ekoontz@hiro-tan.org>
2010-10-18 21:53:18 -0700

    updated Amazon Linux AMI

--- lib/defaults.rb
+++ lib/defaults.rb
@@ -1,7 +1,7 @@
 module Hadoop
   class Defaults
     @@options = {
-      :base_image_name => "ami-04aa5f6d",
+      :base_image_name => "ami-38c33651",
     }
     def Defaults.options
       @@options
