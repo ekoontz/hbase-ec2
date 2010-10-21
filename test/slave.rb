430c96453f15f222d1b19e55d8788dcf60452a38
Eugene Koontz <ekoontz@hiro-tan.org>
2010-10-21 16:41:10 -0700

    start 5 slaves with a hard-wired master address.

---
+++ test/slave.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/ruby
+
+require 'test/unit'
+$:.unshift File.join(File.dirname(__FILE__),"..", "lib")
+require 'Himg.rb'
+require 'hcluster.rb'
+
+include Hadoop
+
+class TestCreateImage < Test::Unit::TestCase
+  def setup 
+    @@global_himg = Himg.new
+  end
+
+  def test1 
+    assert(@@global_himg)
+    assert(@@global_himg.options)
+
+    #required options:
+    # for now, require a specific AMI: (Amazon-branded Redhat.)
+    assert(@@global_himg.options[:base_image_name] == "ami-38c33651")
+    assert(@@global_himg.options[:hbase_roles])
+    assert(@@global_himg.options[:base_image_name])
+
+    @@global_himg.options.keys.each do |key|
+      puts "#{key} => #{@@global_himg.options[key]}\n"
+    end
+
+    5.times { |i|
+      launch = HCluster::do_launch({
+                                     :ami => @@global_himg.options[:base_image_name],
+                                     :key_name => "root",
+                                     :instance_type => "m1.large",
+                                     :on_boot => lambda{|instance|
+                                       slave_hostname = instance.dnsName
+                                       puts "#{slave_hostname}: verifying that sshing to slave works.."
+                                       HCluster::until_ssh_able([instance],0,"ec2-user")
+                                       puts "..ok."
+                                       
+                                       puts "copying slave script.."
+                                       HCluster::scp_to(instance.dnsName,"./lib/puppet/slave/slave.sh","/home/ec2-user/slave.sh","ec2-user")
+                                       puts "..ok."
+                                       
+                                       puts "run slave setup.."
+                                       #run slave setup.
+                                       HCluster::ssh_to(slave_hostname,
+                                                        "sudo sh ./slave.sh",
+                                                        HCluster.echo_stdout,
+                                                        HCluster.echo_stderr,
+                                                        nil,
+                                                        nil,
+                                                        "ec2-user")
+                                       puts "..ok."
+                                     }
+                                   },"start_slave_"+i.to_s)
+      assert(launch[0])
+    }
+
+
+  end
+
+end
+
