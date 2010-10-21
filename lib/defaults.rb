73105f8a8fab57901a581ec62e64b098cccf6010
Eugene Koontz <ekoontz@hiro-tan.org>
2010-10-21 16:41:29 -0700

    remove unneeded java_url.

--- lib/defaults.rb
+++ lib/defaults.rb
@@ -1,8 +1,7 @@
 module Hadoop
   class Defaults
     @@options = {
-      :base_image_name => "ami-38c33651",
-      :java_url => "http://ekoontz-tarballs.s3.amazonaws.com/jdk-6u22-linux-x64.bin"
+      :base_image_name => "ami-38c33651"
     }
     def Defaults.options
       @@options
