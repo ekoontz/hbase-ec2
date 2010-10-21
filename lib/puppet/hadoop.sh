1e1647692ab83f8c0a2af0642d049a6b92018aba
Eugene Koontz <ekoontz@hiro-tan.org>
2010-10-21 13:36:31 -0700

    setting env and path in site.pp, not hadoop.sh.

--- lib/puppet/hadoop.sh
+++ lib/puppet/hadoop.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-# hadoop.sh version 11: start hadoop in relative dir.
-# 
-rm -rf /opt/hadoop-common 
+# hadoop.sh version 16: running as ec2-user.
+#
+rm -rf /opt/hadoop-common
 tar -C /opt -xzf /opt/hadoop-common.tar.gz
-touch /opt/do_service_here.txt
-
-
-
+cd /opt/hadoop-common
+bin/hadoop datanode >> /tmp/hadoop.err 2>&1 &
--- lib/puppet/manifests/site.pp
+++ lib/puppet/manifests/site.pp
@@ -1,5 +1,5 @@
 class tm {
-  
+
   file { "/opt/hadoop-common.tar.gz":
     owner => ec2-user,
     group => ec2-user,
@@ -15,15 +15,33 @@ class tm {
     ignore => ".git*",
     source => "puppet://puppet/files/hadoop.sh"
   }
-  
+
   exec { "/opt/hadoop.sh":
+    user => "ec2-user",
     cwd => "/opt",
-    path => ["/bin","/usr/bin","/usr/sbin"],
-    subscribe => File["/opt/hadoop-common.tar.gz","/opt/hadoop.sh"],
+    path => ["/opt/jdk1.6.0_22/bin","/bin","/usr/bin","/usr/sbin"],
+    subscribe => File["/opt/hadoop-common.tar.gz","/opt/hadoop.sh","/opt/jdk.tar.gz"],
+    environment => ["JAVA_HOME=/opt/jdk1.6.0_22"],
     refreshonly => true
   }
-  
+
+  file { "/opt/jdk.tar.gz":
+    owner => ec2-user,
+    group => ec2-user,
+    mode => 755,
+    ignore => ".git*",
+    source => "puppet://puppet/files/jdk.tar.gz"
+  }
+
+  exec { "tar xfz /opt/jdk.tar.gz":
+    user => "ec2-user",
+    cwd => "/opt",
+    path => ["/bin","/usr/bin","/usr/sbin"],
+    subscribe => File["/opt/jdk.tar.gz"],
+    refreshonly => true
+  }
+
+
 }
 
 include tm
-
