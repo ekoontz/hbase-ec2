12bdb1780e09c742e87410a03c8f082823c057b7
Eugene Koontz <ekoontz@hiro-tan.org>
2010-10-21 16:43:44 -0700

    site manifest and config file; needs to be in /etc/puppet/manifests

---
+++ lib/puppet/site.pp
@@ -0,0 +1,39 @@
+class tm {
+  
+  file { "/opt/hadoop-common.tar.gz":
+    owner => ec2-user,
+    group => ec2-user,
+    mode => 750,
+    ignore => ".git*",
+    source => "puppet://puppet/files/hadoop-common.tar.gz"
+  }
+
+  file { "/opt/hadoop.sh":
+    owner => ec2-user,
+    group => ec2-user,
+    mode => 755,
+    ignore => ".git*",
+    source => "puppet://puppet/files/hadoop.sh"
+  }
+  
+  exec { "/opt/hadoop.sh":
+    user => "ec2-user",
+    cwd => "/opt",
+    path => ["/opt/jre1.6.0_22/bin","/bin","/usr/bin","/usr/sbin"],
+    subscribe => File["/opt/hadoop-common.tar.gz","/opt/hadoop.sh","/opt/jre.tar.gz"],
+    environment => ["JAVA_HOME=/opt/jre1.6.0_22"],
+    refreshonly => true
+  }
+ 
+  file { "/opt/jre.tar.gz":
+    owner => ec2-user,
+    group => ec2-user,
+    mode => 755,
+    ignore => ".git*",
+    source => "puppet://puppet/files/jre.tar.gz"
+  }
+  
+}
+
+include tm
+
