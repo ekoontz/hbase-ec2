a0b35970fd0c168644e17476e12ca4bd57ac99ba
Eugene Koontz <ekoontz@hiro-tan.org>
2010-10-22 15:54:47 -0700

    only start puppet master here (not client)

--- lib/puppet/master.sh
+++ lib/puppet/master.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
-sudo yum -y install puppet-server puppet screen git emacs ant
+sudo yum -y install puppet-server screen git emacs ant
 
 cd ~
 wget -O jdk.bin "http://ekoontz-tarballs.s3.amazonaws.com/jdk-6u22-linux-x64.bin"
@@ -12,6 +12,11 @@ export PATH=$JAVA_HOME:$PATH
 wget http://ekoontz-tarballs.s3.amazonaws.com/apache-maven-3.0-bin.tar.gz
 tar xfz apache-maven-3.0-bin.tar.gz
 export PATH=`pwd`/apache-maven-3.0/bin:$JAVA_HOME:$PATH
+
+git clone git://github.com/ekoontz/hbase-ec2.git
+cd hbase-ec2
+git checkout puppet
+cd ~
 
 git clone git://github.com/trendmicro/hadoop-common.git
 cd hadoop-common
@@ -48,14 +53,17 @@ tar  --exclude=".git*" -czf /tmp/puppetfiles/hadoop-common.tar.gz hadoop-common
 tar  --exclude=".git*" -czf /tmp/puppetfiles/hbase.tar.gz hbase
 tar -czf /tmp/puppetfiles/jre.tar.gz jre1.6.0_22
 tar -czf /tmp/puppetfiles/m2.tar.gz .m2
+set +x
 
-cp ~/puppet/hadoop.sh /tmp/puppetfiles
 
-#start up puppet server and client.
-#cp files to /etc/puppet/puppetmaster.conf, /etc/puppet/fileserver.conf and /etc/puppet/manifests/site.pp.
+cp ~/hbase-ec2/lib/puppet/hadoop.sh /tmp/puppetfiles
+
+#start up puppet server
+sudo cp hbase-ec2/lib/puppet/puppet.conf /etc/puppet/
+sudo cp hbase-ec2/lib/puppet/manifests/site.pp /etc/puppet/manifests/
+sudo cp hbase-ec2/lib/puppet/fileserver.conf /etc/puppet
 
 sudo /etc/init.d/puppetmaster start
-sudo /etc/init.d/puppet start
-PUPPET_MASTER_IP=`hostname -f`
-sudo "echo \"$PUPPET_MASTER_IP puppet puppet.ec2.foofers.org\" | cat >> /etc/hosts"
+export PUPPET_MASTER_IP=`/sbin/ifconfig eth0 | grep "inet addr" | cut -d: -f2-2 | cut -d' ' -f1`
+sudo sh hbase-ec2/lib/puppet/slave.sh $PUPPET_MASTER_IP
 
