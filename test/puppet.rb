cdf32e81b841b0e0607256a5f04fef8474f2c644
Eugene Koontz <ekoontz@hiro-tan.org>
2010-11-08 12:18:19 -0800

    use HCluster::launchp for test

--- test/puppet.rb
+++ test/puppet.rb
@@ -22,81 +22,18 @@ class TestCreateImage < Test::Unit::TestCase
     assert(@@global_himg.options[:hbase_roles])
     assert(@@global_himg.options[:base_image_name])
 
-    @@global_himg.options.keys.each do |key|
-      puts "#{key} => #{@@global_himg.options[key]}\n"
-    end
+#    @@global_himg.options.keys.each do |key|
+#      puts "#{key} => #{@@global_himg.options[key]}\n"
+#    end
 
-    launch = HCluster::do_launch({
-                                   :ami => @@global_himg.options[:base_image_name],
-                                   :key_name => "root",
-                                   :security_group => "all open",
-                                   :instance_type => "m1.large",
-                                   :on_boot => lambda{|instance|
-                                     master_hostname = instance.dnsName
-                                     puts "#{master_hostname}: verifying that sshing works.."
-                                     HCluster::until_ssh_able([instance],0,"ec2-user")
-                                     puts "..ok."
-                                     HCluster::scp_to(master_hostname,"./lib/puppet/master.sh","/home/ec2-user/master.sh","ec2-user")
-                                     HCluster::ssh_to(master_hostname,"sh /home/ec2-user/master.sh",
-                                                      HCluster.echo_stdout,
-                                                      HCluster.echo_stderr,
-                                                      nil,nil,
-                                                      "ec2-user")
-                                     
-                                     #check to make sure puppetmaster (and puppet) are running on master host.
+    cluster = HCluster.new :label => 'amzn-ami-0.9.9-beta.x86_64-ebs'
+    cluster.launchp(:slaves => 0)
 
-                                   }
-                                 },"master")
-
-
-    if (launch && launch[0])
-      @master = launch[0]
-    else
-      raise "Could not launch master."
-    end
-
-    assert(@master)
-    
-    #set up slaves.
-    puppetmaster_private_ip = @master.privateIpAddress
-    
-    @slaves = []
-
-    2.times { |i|
-      launch = HCluster::do_launch({
-                                     :ami => @@global_himg.options[:base_image_name],
-                                     :key_name => "root",
-                                     :instance_type => "m1.large",
-                                     :security_group => "all open",
-                                     :on_boot => lambda{|instance|
-                                       slave_hostname = instance.dnsName
-                                       puts "#{slave_hostname}: verifying that sshing to slave works.."
-                                       HCluster::until_ssh_able([instance],0,"ec2-user")
-                                       puts "..ok."
-                                       
-                                       puts "copying slave script.."
-                                       HCluster::scp_to(instance.dnsName,"./lib/puppet/slave.sh","/home/ec2-user/slave.sh","ec2-user")
-                                       puts "..ok."
-                                       
-                                       puts "run slave setup.."
-                                       #run slave setup.
-                                       HCluster::ssh_to(slave_hostname,
-                                                        "sudo sh ./slave.sh '"+puppetmaster_private_ip+"'",
-                                                        HCluster.echo_stdout,
-                                                        HCluster.echo_stderr,
-                                                        nil,
-                                                        nil,
-                                                        "ec2-user")
-                                       puts "..ok."
-                                     }
-                                   },"start_slave_"+i.to_s)
-      assert(launch[0])     
-      @slaves.push launch[0]
-    }
+    assert(cluster.dnsName)
 
     #wait for hbase to come up, and run some hadoop and hbase tests.
-    puts "master hostname " + @master.dnsName + " ; private IP: " + puppetmaster_private_ip
-    @slaves.each {|slave|
+    puts "master hostname " + cluster.dnsName
+    cluster.slaves.each {|slave|
       puts " slave: " + slave.dnsName
     }
 
